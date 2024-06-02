13cc73e9928ab0ac1edf87f097c8b18513a706b7
Bilal CIFCI <cifci.bilal@outlook.com>
2024-06-02 18:20:28 +0300

    degisken kavrami

---
+++ JavaScriptIleProgramlama/jsDosyalar/8_96_degiskenler.js
@@ -0,0 +1,14 @@
+// var ad_soyad = "Bilisim Dunyasi";
+// alert(ad_soyad +" "+ typeof(ad_soyad));
+
+// var not_bilgisi = 55;
+// alert(not_bilgisi +" "+ typeof(not_bilgisi));
+
+// var x;
+// alert(x +" "+ typeof(x));
+
+// var x = false;
+// alert(x +" "+ typeof(x));
+
+var ortalamalar = [4,4,4,4,423,42,432,23];
+alert(ortalamalar +" "+ typeof(ortalamalar));
