7c0082bc19a765f21d6e70954d51900b8b1937db
Bilal CIFCI <cifci.bilal@outlook.com>
2024-06-03 00:06:36 +0300

    form olaylari 1

---
+++ JavaScriptIleProgramlama/jsDosyalar/8_101_formOlaylari.js
@@ -0,0 +1,19 @@
+// onClick
+// onDblClick
+// onLoad
+// onUnload
+// onFocus
+// onBlur
+// onMouseOver
+// onMouseOut
+// onMouseMove
+// onMouseDown
+// onMouseUp
+// onKeyDown
+// onSelect
+// onResize
+// onSubmit
+
+function olay() {
+    document.getElementById("mesaj").innerHTML = "resim yuklendi";
+}
