149c52e5681f939888d9eedbec14de6bd7514ae6
Bilal CIFCI <cifci.bilal@outlook.com>
2024-06-02 18:11:05 +0300

    yazdirma islemleri

---
+++ JavaScriptIleProgramlama/jsDosyalar/8_95_yazdirmai-islemleri.js
@@ -0,0 +1,17 @@
+//document.getElementById("metin").innerHTML = "Javascript Dersleri";
+
+// function mesaj() {
+//     document.write("Javascript derslerine hosgeldiniz.");
+// }
+// function mesaj() {
+//     alert("Javascript derslerine hosgeldiniz.");
+// }
+// function mesaj() {
+//     alert("Javascript derslerine hosgeldiniz.");
+// }
+// function mesaj() {
+//     document.getElementById("text").value = "Adiniz";
+// }
+// function mesaj() {
+//     console.log("Merhaba JS");
+// }
